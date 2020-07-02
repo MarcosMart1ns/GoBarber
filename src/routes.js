74375942beacba67b47bd8757c0bd1571afbd1b6
Marcos Martins <marcos_36ma@hotmail.com>
2020-07-02 11:57:26 -0300

    'added authorization'

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,20 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 
+import authMiddleware from './app/middlewares/auth'
+
 const routes = new Router();
 
 routes.get('/',(req,res)=>{
     return res.send({message:"funfou!!"});
 })
 
-routes.post('/users', UserController.store)
-routes.post('/session', SessionController.store)
+routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
 
+routes.use(authMiddleware);
 
+routes.put('/users',UserController.update);
 
 export default routes;
 
