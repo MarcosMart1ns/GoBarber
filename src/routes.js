c6562398dcb329a792f81ea93cda96c2b06734f7
Marcos Martins <marcos_36ma@hotmail.com>
2020-07-02 10:40:08 -0300

    'added model e controller'

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,13 @@
 import { Router } from 'express';
-import User from './app/models/User;'
+import UserController from './app/controllers/UserController'
 
 const routes = new Router();
 
 routes.get('/',(req,res)=>{
-
     return res.send({message:"funfou!!"});
-
 })
 
-routes.get('/insert',async (req,res)=>{
-
-    const user = await User.create({
-        name: 'Marcos Martins',
-        email: 'marcos_36masdas@hotmail.com',
-        password_hash: '928193uqjaisjd',
-        provider: false
-    })
-
-    return res.send({user})
-})
+routes.post('/users', UserController.store)
 
 
 
